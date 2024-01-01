624ead48fa7b9f911826705495bebe6a367e4741
kok shen <kokshen3414@gmail.com>
2024-01-01 10:34:56 +0800

    did a lot of changes

--- login_register_php/upload_resume.php
+++ login_register_php/upload_resume.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload a resume</title>
 </head>
+
 <body>
-    <form action="upload.php" method="POST" enctype="multipart/form-data"></form>
-    <input type="file" name="file">
-    <button type="submit" name="submit">Upload</button>
+    <form action="uploads.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <button type="submit" name="submit">Upload</button>
+    </form>
+
 </body>
+
 </html>
